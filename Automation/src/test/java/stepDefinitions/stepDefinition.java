4587d1b98e1a39664eea5c1118f1827b563fc098
aornelasc <aornelasc@hotmail.com>
2021-08-02 18:58:10 -0500

    ornelas

--- Automation/src/test/java/stepDefinitions/stepDefinition.java
+++ Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -1,29 +1,6 @@
 package stepDefinitions;
 
-import org.junit.runner.RunWith;
-
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.cucumber.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-public class stepDefinition
-{
-	@Given("^User is on NetBanking landing page$")
-    public void user_is_on_netbanking_landing_page() throws Throwable 
-	{
-		System.out.println("User is on NetBanking landing page");
-       
-    }
-
-	@When("^User logins into application with \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void user_logins_into_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable 
-	{
-       
-		System.out.println("User logins into application with "+ strArg1 +" and " +strArg2);
-    }
+im
 
 
     @Then("^Home page is populate$")
